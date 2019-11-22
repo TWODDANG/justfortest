6d5a5dcf87e83fece5a7b3613e98abc885913825
2dd <79iug12@gmail.com>
2019-11-23 00:47:11 +0900

    asd

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ require('dotenv').config();
 const app = express();
 if(process.env.NODE_ENV === 'production'){
     const path = require('path');
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+        res.sendFile(path.join(__dirname+'../client/build/index.html'));
     });
 }
 
